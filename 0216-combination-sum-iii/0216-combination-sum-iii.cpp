b9e89e0b06c6ea159c0e9fb7f075e31a3c680975
Deepesh Kumar Meena <deepeshmeena2104232@dei.ac.in>
2026-07-02 22:58:58 +0530

    Time: 0 ms (100%), Space: 8.8 MB (42.17%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.cpp
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -1,26 +1,31 @@
 class Solution {
 public:
 
-vector<vector<int>>ans;
-void deep(int i ,int sum ,vector<int>&arr, int n , int k ,vector<int>&result){
+    vector<vector<int>> ans;
+    void solve(int start, int k, int target, vector<int>& curr) {
 
-    if(i == arr.size()){
-        if(sum == n && result.size() == k)
-            ans.push_back(result);
-        return;
+        if (target == 0 && curr.size() == k) {
+            ans.push_back(curr);
+            return;
+        }
+
+        if (target < 0 || curr.size() > k)
+            return;
+
+        for (int i = start; i <= 9; i++) {
+
+            curr.push_back(i);
+            solve(i + 1, k, target - i, curr);
+            curr.pop_back();
+        }
     }
 
-    if(sum+arr[i]<=n){
-        result.push_back(arr[i]);
-        deep(i+1,sum+arr[i],arr,n,k,result);
-        result.pop_back() ;
-    }
-    deep(i+1,sum,arr,n,k,result);
-}
     vector<vector<int>> combinationSum3(int k, int n) {
-        vector<int> arr = {1,2,3,4,5,6,7,8,9};
-        vector<int>result;
-        deep(0,0,arr,n,k,result);
-        return ans ;
+
+        vector<int> curr;
+
+        solve(1, k, n, curr);
+
+        return ans;
     }
 };
