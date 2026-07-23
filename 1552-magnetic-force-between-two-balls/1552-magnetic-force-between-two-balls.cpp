a8906163cde162e7eb7402e74f2d24e7c3edd5f1
Deepesh Kumar Meena <deepeshmeena2104232@dei.ac.in>
2026-07-23 19:23:45 +0530

    Time: 43 ms (88.9%), Space: 61.6 MB (70.18%) - LeetHub

--- 1552-magnetic-force-between-two-balls/1552-magnetic-force-between-two-balls.cpp
+++ 1552-magnetic-force-between-two-balls/1552-magnetic-force-between-two-balls.cpp
@@ -1,47 +1,41 @@
 class Solution {
 public:
-    
-    bool possible(vector<int>&position , int m , int dis){
 
-        int count_ball = 1 , last = position[0] ;
-        bool flag = false ;
-        for(int i = 1 ; i<position.size() ; i++){
-            if(position[i]-last>=dis){
-                    count_ball++;
-                    last=position[i] ; 
-                }
+    bool possible(vector<int>& position, int m, int dis){
+
+        int count_ball = 1, last = position[0];
+
+        for(int i = 1; i < position.size(); i++){
+            if(position[i] - last >= dis){
+                count_ball++;
+                last = position[i];
             }
-            if(count_ball>=m) {
-                flag = true ; 
-                }   
-            return flag ;
         }
 
-
-    int maxim(vector<int>&position){
-        int maxi = 0 ;
-        for(auto it : position){
-            maxi = max(maxi,it);
-        }
-        return maxi ;
+        return count_ball >= m;
     }
 
     int maxDistance(vector<int>& position, int m) {
 
-        sort(position.begin(),position.end());
+        sort(position.begin(), position.end());
+
         int maxDis = position.back() - position.front();
-        int low = 1 , high = maxim(position) , ans = maxDis ;
-        
-        while(low<=high){
-            int mid = low+(high-low)/2 ;
-            if(possible(position,m,mid)==false){
-                ans = mid-1;
-                high = mid-1;
+
+        int low = 1, high = maxDis, ans = 0;
+
+        while(low <= high){
+
+            int mid = low + (high - low) / 2;
+
+            if(possible(position, m, mid)){
+                ans = mid;
+                low = mid + 1;
             }
             else{
-                low=mid+1;
+                high = mid - 1;
             }
         }
-        return ans ;
+
+        return ans;
     }
 };
