6aac882c1edbd0f2e425904dc8099e5835d6be3a
Deepesh Kumar Meena <deepeshmeena2104232@dei.ac.in>
2026-07-26 19:51:19 +0530

    Time: 63 ms (37.73%), Space: 151.2 MB (54.25%) - LeetHub

---
+++ 0155-min-stack/0155-min-stack.cpp
@@ -0,0 +1,30 @@
+class MinStack {
+
+    stack<pair<int,int>> st;
+
+public:
+    MinStack() {
+    }
+
+    void push(int value) {
+
+        if(st.empty()){
+            st.push({value, value});
+        }
+        else{
+            st.push({value, min(value, st.top().second)});
+        }
+    }
+
+    void pop() {
+        st.pop();
+    }
+
+    int top() {
+        return st.top().first;
+    }
+
+    int getMin() {
+        return st.top().second;
+    }
+};
