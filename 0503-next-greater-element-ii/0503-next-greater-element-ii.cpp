f6e61334b9ed046182b5aebabdf2c6a5e0498432
Deepesh Kumar Meena <deepeshmeena2104232@dei.ac.in>
2026-07-28 01:01:57 +0530

    Time: 7 ms (35.46%), Space: 28.4 MB (84.41%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -4,20 +4,21 @@ public:
 
         int n = nums.size();
         vector<int> nge(n, -1);
-
-        for(int i = 0; i < n; i++) {
-
-            for(int j = i + 1; j < i + n; j++) {
-
-                int ind = j % n;
-
-                if(nums[ind] > nums[i]) {
-                    nge[i] = nums[ind];
-                    break;
+        stack<int>st;
+        for(int i = 2*n -1 ; i >= 0; i--){
+            while(!st.empty() && st.top()<=nums[i%n]){
+                st.pop();
+            }
+            if(i<n){
+                if(st.empty()){
+                    nge[i]=-1;
+                }
+                else{
+                    nge[i]=st.top();
                 }
             }
-        }
-
-        return nge;
+            st.push(nums[i%n]);
+        } 
+        return nge ;
     }
 };
