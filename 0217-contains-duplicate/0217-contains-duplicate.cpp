a434dae6deada9f1652ffbf1b613b1bd04d12ff3
Deepesh Kumar Meena <deepeshmeena2104232@dei.ac.in>
2026-08-04 09:43:01 +0530

    Time: 78 ms (23.36%), Space: 111.2 MB (40.38%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -4,12 +4,10 @@ public:
         unordered_map<int,int>mpp;
 
         for(int i = 0 ; i<nums.size() ; i++){
-            mpp[nums[i]]++;
-        }
-        for(auto it : mpp){
-            if(it.second>1){
+           if(mpp.find(nums[i])!=mpp.end()){
                 return true ;
             }
+            mpp[nums[i]]++;
         }
         return false ;
     }
