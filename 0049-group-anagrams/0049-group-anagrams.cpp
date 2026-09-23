91fcc5472e08deecb5e5958ddce1f7b3f09b9f74
Deepesh Kumar Meena <deepeshmeena2104232@dei.ac.in>
2026-09-23 21:58:50 +0530

    Time: 21 ms (34.16%), Space: 27.3 MB (22.99%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,5 +1,25 @@
 class Solution {
 public:
+
+    string generate(string &word){
+        int arr[26]={0};
+
+        for(char &ch : word){
+            arr[ch-'a']++ ;
+        }
+
+        string new_word ="";
+        
+        for(int i = 0 ; i<26 ; i++){
+            int freq = arr[i];
+
+            if(freq>0){
+                new_word += string(freq,i+'a');
+            }
+        }
+        return new_word ;
+    }
+
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         
         vector<vector<string>> ans ;
@@ -12,8 +32,8 @@ public:
 
         for(int i = 0 ; i< n ; i++){
             string temp = strs[i] ;
-            sort(begin(temp), end(temp)) ;            
-            mpp[temp].push_back(strs[i]);
+            string new_word = generate(temp);
+            mpp[new_word].push_back(temp);
         }
         
         for(auto it : mpp){
