d81eb49e1c34f88f20d687b3c94b9141a2451a0a
Deepesh Kumar Meena <deepeshmeena2104232@dei.ac.in>
2026-09-23 21:19:14 +0530

    Time: 15 ms (69.43%), Space: 26.2 MB (44.87%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        
+        vector<vector<string>> ans ;
+
+        int n = strs.size() ;
+
+        unordered_map<string,vector<string>> mpp ;
+
+
+
+        for(int i = 0 ; i< n ; i++){
+            string temp = strs[i] ;
+            sort(begin(temp), end(temp)) ;            
+            mpp[temp].push_back(strs[i]);
+        }
+        
+        for(auto it : mpp){
+            ans.push_back(it.second);
+        }
+        return ans ;
+    }
+};
